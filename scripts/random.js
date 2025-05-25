ec437edb5bd939036250be6797e1a679c879ec24
Saneec <crisman2005@gmail.com>
2025-05-25 19:36:30 -0400

    Initial Commit
    Easter egg.

--- scripts/random.js
+++ scripts/random.js
@@ -2,4 +2,5 @@ function createRandomInt() {
     const num = Math.floor(Math.random() * 10000);
     const output = document.getElementById("output");
     output.textContent = `Your number: ${num}`;
+    console.log("Balls");
 }
