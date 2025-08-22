c62684481b5c6080b168325fc4ff0fc6dfdfc6d4
Saneec <crisman2005@gmail.com>
2025-08-22 02:35:41 -0400

    (BROKEN) memory to draggable elements
    Added a drag function that once finished, returns the element to it's original spot.
    Does not work currently. It fails to detect when the element is left clicked again, and does not return to original position.

--- js/moveCards.js
+++ js/moveCards.js
@@ -12,7 +12,7 @@ function updateCards() {
         let dragFunction = function(event)  {
             mouseX = event.clientX;
             mouseY = event.clientY;
-            dragElement(card);
+            dragElementWithMemory(event, card);
             
         }
         // let EventRemover = function() {
@@ -40,13 +40,11 @@ function dragElement(element) {
             element.removeEventListener("mousemove", moveThisElement);
             element.removeEventListener("mouseup", EventRemover);
         };
-        element.addEventListener("mouseup", EventRemover);
-        
-        
-    };
-    
-function moveElement(element) {
-    return function (event) {
+        element.addEventListener("mouseup", EventRemover);   
+    }
+
+    function moveElement(element) {
+        return function (event) {
         let mouseDisplacementX = event.clientX - mouseX;
         let mouseDisplacementY = event.clientY - mouseY;
         
@@ -54,5 +52,45 @@ function moveElement(element) {
         mouseY = event.clientY;
         element.style.left = mouseDisplacementX + element.offsetLeft + "px";
         element.style.top = mouseDisplacementY + element.offsetTop + "px";
+        };
+    }
+    function dragElementWithMemory(event, element) {
+        let originalLeft = element.style.left;
+        let originalTop = element.style.top;
+        const moveThisElement = moveElement(element);
+        element.addEventListener("mousemove", moveThisElement);
+        let EventRemover = function() {
+            element.removeEventListener("mousemove", moveThisElement);
+            element.removeEventListener("mouseup", EventRemover);
+            setInterval( function() {
+                console.log("The function has triggered!");
+                approachPositionRecursively(element, event, originalLeft,originalTop,element.style.left,element.style.top);
+            })
+        };
+        element.addEventListener("mouseup", EventRemover);
     };
-}
+
+    function approachPositionRecursively(element, event, originalLeft, originalTop, currentLeft, currentTop) {
+        console.log("Doing my job")
+        //In the future, it can look better if they change based on the ratio between the current left and top positions (hypotenuse)
+        let originalLeftDifference = originalLeft - currentLeft;
+        let originalTopDifference = originalTop - currentTop;
+
+        //can a case statement be used instead?
+
+        //refer to docs to detect when left click is pressed along with other buttons
+        if((originalLeftDifference == 0 && originalTopDifferent == 0) || event.buttons == 1)
+            return;
+        if(originalLeftDifference < 0)
+            element.style.left = element.style.left + 1 + "px";
+        else if (originalLeftDifference > 0)
+            element.style.left = element.style.left - 1 + "px";
+        if(originalTopDifference < 0)
+            element.style.top = element.style.top + 1 + "px";
+        else if (originalTopDifference > 0)
+            element.style.top = element.style.top - 1 + "px";
+        setInterval( function() {
+                approachPositionRecursively(element, originalLeft,originalTop,element.style.left,element.style.top);
+            })
+
+    }
