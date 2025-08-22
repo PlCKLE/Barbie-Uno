773e082bf17311adb88763e538a698b8c238e693
Saneec <crisman2005@gmail.com>
2025-08-22 15:40:32 -0400

    Fixed Drag with Memory
    It will now return to where it once was. Bug does exist when you interrupt it's drag. Will be fixed when implemented as a class

--- js/moveCards.js
+++ js/moveCards.js
@@ -4,6 +4,8 @@ let mouseY = 0;
 //This can probably be made into a class
 //Actually, it very likely needs to be integrated within a card class. EventRemover for dragFunction should be callable for each individual card.
 
+
+//Double events can be avoided when cards are objectified and have a "isDragging" boolean with the event adder in an if statement.
 function updateCards() {
     let cards = document.getElementsByClassName("Card");
 
@@ -12,7 +14,9 @@ function updateCards() {
         let dragFunction = function(event)  {
             mouseX = event.clientX;
             mouseY = event.clientY;
-            dragElementWithMemory(event, card);
+            let originalLeftPx = card.style.left;
+            let originalTopPx = card.style.top;
+            dragElementWithMemory(event, card, originalLeftPx, originalTopPx);
             
         }
         // let EventRemover = function() {
@@ -54,43 +58,56 @@ function dragElement(element) {
         element.style.top = mouseDisplacementY + element.offsetTop + "px";
         };
     }
-    function dragElementWithMemory(event, element) {
-        let originalLeft = element.style.left;
-        let originalTop = element.style.top;
+    function dragElementWithMemory(event, element,originalLeftPx,originalTopPx) {
         const moveThisElement = moveElement(element);
         element.addEventListener("mousemove", moveThisElement);
+
         let EventRemover = function() {
             element.removeEventListener("mousemove", moveThisElement);
             element.removeEventListener("mouseup", EventRemover);
-            setInterval( function() {
-                console.log("The function has triggered!");
-                approachPositionRecursively(element, event, originalLeft,originalTop,element.style.left,element.style.top);
+
+            setTimeout( function() {
+                approachPositionRecursively(element, event, parseInt(originalLeftPx),parseInt(originalTopPx));
             })
+
         };
         element.addEventListener("mouseup", EventRemover);
     };
 
-    function approachPositionRecursively(element, event, originalLeft, originalTop, currentLeft, currentTop) {
-        console.log("Doing my job")
+    function approachPositionRecursively(element, event, positionLeft, positionTop) {
         //In the future, it can look better if they change based on the ratio between the current left and top positions (hypotenuse)
-        let originalLeftDifference = originalLeft - currentLeft;
-        let originalTopDifference = originalTop - currentTop;
+        let currentLeft = parseInt(element.style.left);
+        let currentTop = parseInt(element.style.top);
+        let originalLeftDifference = positionLeft - currentLeft;
+        let originalTopDifference = positionTop - currentTop;
+        
 
         //can a case statement be used instead?
 
         //refer to docs to detect when left click is pressed along with other buttons
-        if((originalLeftDifference == 0 && originalTopDifferent == 0) || event.buttons == 1)
+        if((originalLeftDifference == 0 && originalTopDifference == 0)) {
+            console.log("Returning!");
             return;
-        if(originalLeftDifference < 0)
-            element.style.left = element.style.left + 1 + "px";
-        else if (originalLeftDifference > 0)
-            element.style.left = element.style.left - 1 + "px";
-        if(originalTopDifference < 0)
-            element.style.top = element.style.top + 1 + "px";
-        else if (originalTopDifference > 0)
-            element.style.top = element.style.top - 1 + "px";
-        setInterval( function() {
-                approachPositionRecursively(element, originalLeft,originalTop,element.style.left,element.style.top);
+        }
+        if(originalLeftDifference < 0) {
+            element.style.left = currentLeft - 1 + "px";
+            console.log("Moving Right")
+        }
+        else if (originalLeftDifference > 0){
+            element.style.left = currentLeft + 1 + "px";
+            console.log("Moving left")
+        }
+        if(originalTopDifference < 0){
+            element.style.top = currentTop - 1 + "px";
+            console.log("Moving up")
+        }
+        else if (originalTopDifference > 0){
+            element.style.top = currentTop + 1 + "px";
+            console.log("Moving Down")
+        }
+        setTimeout( function() {
+
+                approachPositionRecursively(element, event, positionLeft,positionTop);
             })
 
     }
