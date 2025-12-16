60a0f2d00a1724a87096df5de1fa9c542a160a38
Saneec <crisman2005@gmail.com>
2025-12-16 18:46:18 -0500

    Updated Card.jsx

--- barbie-uno/src/components/Card.jsx
+++ barbie-uno/src/components/Card.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react"
 
-export function Card() {
-    const [value, setValue] = useState();
-    const [color, setColor] = useState("");
+export function Card({ value, color }) {
+   return (
+     <>
+     
+     </>
+   )
 }
+
