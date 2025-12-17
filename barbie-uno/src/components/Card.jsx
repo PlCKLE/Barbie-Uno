b9bf4fbb1e20e457ba97ec2ecd861d40b4dc4438
Saneec <crisman2005@gmail.com>
2025-12-17 00:40:31 -0500

    Implemented Card Location Return

--- barbie-uno/src/components/Card.jsx
+++ barbie-uno/src/components/Card.jsx
@@ -1,12 +1,17 @@
 import { useEffect, useRef, useState } from "react"
+  let isDragging = true;
 export function Card({ value, color, cardIndex}) {
   const cardRef = useRef(null);
+  const [originalOffsetLeft, setOriginalOffsetLeft] = useState(null);
+  const [originalOffsetTop,setOriginalOffsetTop] = useState(null);
+ 
   useEffect(() => {
-    console.log(cardRef.current)
-    injectDrag(cardRef.current);
-    console.log(cardIndex);
-
+    setOriginalOffsetLeft(cardRef.current.offsetLeft);
+    setOriginalOffsetTop(cardRef.current.offsetTop);
   },[])
+  useEffect(() => {
+    injectDrag(cardRef.current);
+  }, [originalOffsetTop])
    return (
      <div ref={cardRef} className="Card">
         <img width = "200px" height = "200px" src = "https://images.ctfassets.net/l7h59hfnlxjx/582Lx8AhvXHgRLXagk73lV/ef827f6b381202b112b61e218d8e3154/President_Obama_Headshot__Economic_Inclusion___Photo_by_Pari_Dukovic_courtesy_of_Penguin_Random_House_.jpg?q=75&w=1014&fm=webp" />
@@ -20,7 +25,8 @@ export function Card({ value, color, cardIndex}) {
 function injectDrag(card) {
   card.ondragstart = doNothing;
   let dragFunction = function(event)  {
-      dragElementWithMemory(event, card);
+    isDragging = true;
+    dragElementWithMemory(event, card);
   }
   card.addEventListener("mousedown",dragFunction);
 }
@@ -30,64 +36,91 @@ function injectDrag(card) {
     const mouseDifferenceY = parseInt(element.offsetTop) - parseInt(event.clientY);
 
       const moveThisElement = moveElement(element, mouseDifferenceX, mouseDifferenceY);
-      element.addEventListener("mousemove", moveThisElement);
+      document.onmousemove = moveThisElement
       let EventRemover = function() {
-          element.removeEventListener("mousemove", moveThisElement);
-          element.removeEventListener("mouseup", EventRemover);
+          document.onmousemove = null;
+          document.onmouseup = null;
           element.style.zIndex = cardIndex;
+          isDragging = false;
           setTimeout(function() {
-            // approachPositionRecursively(element, parseInt(element.style.left),parseInt(element.style.top));
+            approachPositionRecursively(element);
           })
 
       };
-      element.addEventListener("mouseup", EventRemover);
+      document.onmouseup = EventRemover;
   };
   function moveElement(element, previousMouseDifferenceX, previousMouseDifferenceY) {
     return function (event) {
-    console.log("Attempting move!")
     const currentMouseDifferenceX = parseInt(event.clientX) - parseInt(element.offsetLeft);
     const currentMouseDifferenceY = parseInt(element.offsetTop) - parseInt(event.clientY);
     const mouseDisplacementX = currentMouseDifferenceX - previousMouseDifferenceX;
     const mouseDisplacementY = previousMouseDifferenceY - currentMouseDifferenceY;
-    console.log("Mouse displacement on X: " + mouseDisplacementX)
     element.style.left = mouseDisplacementX + element.offsetLeft + "px";
     element.style.top = mouseDisplacementY + element.offsetTop + "px";
     };
 }
 
-    function approachPositionRecursively(element, positionLeft, positionTop) {
+    //It would be really cool if someone made a formula to smooth out the moving.
+    async function approachPositionRecursively(element) {
         //In the future, it can look better if they change based on the ratio between the current left and top positions (hypotenuse)
+        if(isDragging)
+          return;
         let currentLeft = parseInt(element.offsetLeft);
         let currentTop = parseInt(element.offsetTop);
-        let originalLeftDifference = positionLeft - currentLeft;
-        let originalTopDifference = positionTop - currentTop;
-        
+        let originalLeftDifference = originalOffsetLeft - currentLeft;
+        let originalTopDifference = originalOffsetTop - currentTop;
+        console.log(originalOffsetLeft)
 
         //can a case statement be used instead?
 
         //refer to docs to detect when left click is pressed along with other buttons
         if((originalLeftDifference == 0 && originalTopDifference == 0)) {
-            console.log("Returning!");
             return;
         }
+        const speedMagnitude3 = 30
+        const speedMagnitude2 = 8
+        const speedMagnitude1 = 1
+        const rangeMangitude3 = 20
+        const rangeMangitude2 = 10
+        //Move left or right
         if(originalLeftDifference < 0) {
-            element.style.left = currentLeft - 1 + "px";
-            console.log("Moving Right")
+          if(originalLeftDifference < -rangeMangitude3)
+            element.style.left = currentLeft - speedMagnitude3 + "px"
+          else if(originalLeftDifference < -rangeMangitude2)
+            element.style.left = currentLeft - speedMagnitude2 + "px"
+          else
+            element.style.left = currentLeft - speedMagnitude1 + "px";
+          
         }
         else if (originalLeftDifference > 0){
-            element.style.left = currentLeft + 1 + "px";
-            console.log("Moving left")
+          if(originalLeftDifference > rangeMangitude3)
+            element.style.left = currentLeft + speedMagnitude3 + "px"
+          else if(originalLeftDifference > rangeMangitude2)
+            element.style.left = currentLeft + speedMagnitude2 + "px"
+          else
+            element.style.left = currentLeft + speedMagnitude1 + "px";
+
         }
+
+        //Move up or down
         if(originalTopDifference < 0){
-            element.style.top = currentTop - 1 + "px";
-            console.log("Moving up")
+          if(originalTopDifference < -rangeMangitude3)
+            element.style.top = currentTop - speedMagnitude3 + "px";
+          else if (originalTopDifference < -rangeMangitude2)
+            element.style.top = currentTop - speedMagnitude2 + "px";
+          else 
+            element.style.top = currentTop - speedMagnitude1 + "px";
         }
         else if (originalTopDifference > 0){
-            element.style.top = currentTop + 1 + "px";
-            console.log("Moving Down")
+          if(originalTopDifference > rangeMangitude3)
+            element.style.top = currentTop + speedMagnitude3 + "px";
+          else if(originalTopDifference > rangeMangitude2)
+            element.style.top = currentTop + speedMagnitude2 + "px";
+          else 
+            element.style.top = currentTop + speedMagnitude1 + "px";  
         }
-        setTimeout(approachPositionRecursively(element,positionLeft,positionTop), 25);
-        return;
+        await new Promise(r => setTimeout(r, 8));
+        approachPositionRecursively(element,originalOffsetTop,originalOffsetLeft);
 
     }
 }
--- barbie-uno/src/pages/Game/Game.jsx
+++ barbie-uno/src/pages/Game/Game.jsx
@@ -6,6 +6,9 @@ export function Game() {
             <Card value = {1} color = "blue" key = {2} cardIndex = {2} />
             <Card value = {1} color = "blue" key = {3} cardIndex = {3} />
             <Card value = {1} color = "blue" key = {4} cardIndex = {4} />
+            <Card value = {1} color = "blue" key = {5} cardIndex = {5} />
+            <Card value = {1} color = "blue" key = {6} cardIndex = {6} />
+            <Card value = {1} color = "blue" key = {7} cardIndex = {7} />
         </div>
     )
 }
